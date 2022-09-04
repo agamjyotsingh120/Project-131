f1049e0013a45d0e111cde6fca52621ad7863b5b
Agamjyot Singh <80823915+agamjyotsingh120@users.noreply.github.com>
2022-09-04 20:41:25 +0530

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,33 @@
+Status = "";
+phone_image = "";
+
+function preload(){
+    phone_image = loadImage("phone.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(phone_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(phone_image,0,0,640,350);
+}
+function phone(){
+    window.location = "index.html";
+}
